deb44b1f74fd4a3d3e8a0bfad27b55813b1bd199
vitor martini <vitor-martini@outlook.com>
2022-03-24 14:29:13 -0300

    Reorganizado os arquivos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"; // Importando express
-import db from "./config/dbConnect.js" // Importando conexão com o banco
-import routes from "./routes/index.js"
+import db from "./server/database/dbConnect.js" // Importando conexão com o banco
+import routes from "./server/routes/index.js"
 
 db.on("error", console.log.bind(console, 'Erro de conexão')); // Liga o banco e trata caso dê erro
 db.once("open", () =>{ // Se a conexão foi feita com sucesso, exibe a mensagem
--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,4 @@
-import app from './src/app.js'; // Importanto arquivo app.js
+import app from '../app.js'; // Importanto arquivo app.js
 const port = process.env.PORT || 3000; // Definindo a porta como variável de ambiente (producao) ou 3000 (dev)
 
 app.listen(port, () =>{ //Sobe o servidor
